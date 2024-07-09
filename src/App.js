e172341fcb314ce458b5d2ebc7ca5c006020b1c8
Ko Phone <phonethantzaw@gmail.com>
2024-07-09 15:19:39 -0500

    Students Entity call with http request & response

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-import { Student } from './Student';
+import {Student} from './Student';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
-  const courseList = [
-    { id: 1, name: 'EA', code: '544' },
-    { id: 2, name: 'WAA', code: '525' },
-    { id: 3, name: 'MPP', code: '420' }
-    ];
-  
-    const students = [
-      { id: 1, fname: 'John', lname: 'Doe', email: 'john@miu.edu', major: 'CS', gpa: 3.5, courses: courseList },
-      { id: 2, fname: 'Jane', lname: 'Doe', email: 'jane@miu.edu', major: 'CS', gpa: 3.6, courses: courseList },
-      { id: 3, fname: 'Jimeo', lname: 'Doe', email: 'jime@miu.edu', major: 'CS', gpa: 3.7, courses: courseList }
-  
-    ];
-  
+
+    const students = [];
+
+    const [studentState, setStudentState] = useState(students);
+    const getAllStudents = async () => {
+        const result = await axios.get("http://localhost:8080/students");
+        setStudentState(result.data);
+    }
+
+    useEffect(() => {
+        getAllStudents();
+    }, []);
+
     return (
-      <div className="App">
-  
-  
-        <h2>Student List</h2>
-        {
-          students.map((student)=>{
-            return <Student key={student.id} fname={student.fname} lname={student.lname} email={student.email} major={student.major} gpa={student.gpa} courses={student.courses} />
-          })
-        }
-  
-      </div>
+        <div className="App">
+
+
+            <h2>Student List</h2>
+            {
+                studentState.map((student) => {
+                    return <Student key={student.id} fname={student.firstName} lname={student.lastName}
+                                    email={student.email} major={student.major} gpa={student.gpa}
+                                    courses={student.coursesTaken}/>
+                })
+            }
+
+        </div>
     );
 }
 
